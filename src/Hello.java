0c852573e9d1320afad719eb815a7bd5f4b3f418
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-13 18:21:27 -0500

    defining variables in java

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,17 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello Yogesh");
+
+        //Defining variables in java
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber *2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastone = 1000 - myTotal;
+        System.out.println(myFirstNumber);
+        System.out.println(mySecondNumber);
+        System.out.println(myThirdNumber);
+        System.out.println(myTotal);
+        System.out.println(myLastone  );
     }
 }
