5ae2219d031f5f89299a318e350cf3fc9cfbfe0f
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-13 21:52:42 -0500

    kilogram to pounds calculator

---
+++ src/KilogramToPounds.java
@@ -0,0 +1,7 @@
+public class KilogramToPounds {
+    public static void main(String[] args) {
+        double pounds = 1L;
+        double kilogramsToPounds =  pounds * 0.45359237;
+        System.out.println("kilograms to pounds is: " + kilogramsToPounds);
+    }
+}
