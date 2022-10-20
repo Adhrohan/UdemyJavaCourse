913ab4aa041309eedd63f3575174586178780492
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-20 16:45:00 -0500

    converting feet and inches to centimeter using method overloading

---
+++ src/MethodOverlaodingExercise.java
@@ -0,0 +1,24 @@
+public class MethodOverlaodingExercise {
+    public static void main(String[] args) {
+        calcFeetAndInchesToCentimeters(5,10);
+        calcFeetAndInchesToCentimeters(6);
+    }
+
+    public static void calcFeetAndInchesToCentimeters(int feet, int inches){
+        if (feet >= 0 || (inches >= 0 && inches <= 12)){
+            double centimeters = feet * 12 * 2.54 + inches;
+            System.out.println(centimeters);
+        }else {
+            System.out.println(-1);
+        }
+    }
+
+    public static void calcFeetAndInchesToCentimeters(int feet){
+        if (feet >= 0 ){
+            double centimeters = feet * 12 * 2.54;
+            System.out.println(centimeters);
+        }else {
+            System.out.println(-1);
+        }
+    }
+}
