508a05acc1f93fcbe74978e264a6196579b27715
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-20 17:17:16 -0500

    java switch statement

---
+++ src/SwitchStatement.java
@@ -0,0 +1,18 @@
+public class SwitchStatement {
+
+    public static void main(String[] args) {
+
+        char value = 'a';
+
+        switch (value){
+            case 'a':
+                System.out.println("value was char a");
+                break;
+            default:
+                System.out.println("value was not a char a");
+
+        }
+
+    }
+
+}
