61962e68486ed143739db6d53397feacbc732a5c
Yogesh Adhikari <yogeshadhikari81@gmail.com>
2022-10-21 17:33:36 -0500

    java constructor Area of a wall

--- src/main.java
+++ src/main.java
@@ -35,6 +35,14 @@ public class main {
         System.out.println(vipCustomer2.getCreditLimit());
         System.out.println(vipCustomer2.getEmail());
 
+        Wall wall = new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
+
 
     }
 }
